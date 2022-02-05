63ad1446a6714ab997f343d647581f32b8e47186
Grant-Stoehr <54612499+Grant-Stoehr@users.noreply.github.com>
2022-02-04 22:27:42 -0600

    Update BirthDeathRatesRegression.py
    Uploaded the proper file

--- BirthDeathRatesRegression.py
+++ BirthDeathRatesRegression.py
@@ -1,45 +1,53 @@
-import numpy as np
 import pandas as pd
+import numpy as np
+from scipy.optimize import curve_fit
+from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 
+# Source: https://stackoverflow.com/questions/60160803/scipy-optimize-curve-fit-for-logistic-function
+def logistic_function(x, a, b, c, d):
+    return a / (1. + np.exp(-c * (x - d))) + b
+
 ##Reading in the data and cleaning it
-with open('PopulationData.csv', 'r') as f:
+with open('BirthsandDeathRates.csv', 'r') as f:
      lines = f.readlines()
-world_data = lines[218].strip('\n')
-world_data = world_data.split(',')
-world_data = world_data[5:]
-for i in range(len(world_data)):
-    world_data[i] = float(world_data[i].replace('"', ''))
-years = list(range(1960, 2021))
+birth_rates =[]
+death_rates = []
+for i in range(51, 73):
+    #print(lines[i])
+    birth_rates.append(float(lines[i].split(',')[2]))
+    death_rates.append(float(lines[i].split(',')[3]))
+years = list(range(2000, 2022))
+
+for i in range(len(birth_rates)):
+    birth_rates[i] = birth_rates[i]/1000000
+
+for i in range(len(death_rates)):
+    death_rates[i] = death_rates[i]/1000000
+
 
 ##Converting the arrays to numpy arrays and reshaping the x-axis as well as flattening the arrays into 1D as opposed to 2D
 years = np.array(years).reshape(-1, 1).flatten()
-world_data = np.array(world_data).flatten()
+birth_rates = np.array(birth_rates).flatten()
+death_rates = np.array(death_rates).flatten()
 
-#Takes the log of the world data
-y_log = np.log(world_data)
+paramsBirth, covariance = curve_fit(logistic_function, years, birth_rates, p0=[2, 2000, .5, 2022]) # p0 is the initial guess for the parameters
+r2 = r2_score(birth_rates, logistic_function(years, *paramsBirth)) # Calculating the R^2 value
+print('R-Squared for births:', r2)
 
-#Fit the data to an exponential function and strip the coefficients
-fit = np.polyfit(world_data, np.log(years), 1)
-curve_fit = np.polyfit(years,y_log, 1)
-firstCoEff,secondCoEff = curve_fit[0],curve_fit[1]
+predictionBirth = logistic_function(2122, *paramsBirth)
+print('Prediction for births in 2122:', round(predictionBirth*1000000), "people")
 
-#exponential_equation = np.exp(secondCoEff) * np.exp(firstCoEff * years) << This is the final Exponential equation
+paramsDeath, covariance = curve_fit(logistic_function, years, birth_rates, p0=[2, 2000, .5, 2022]) # p0 is the initial guess for the parameters
+r2 = r2_score(birth_rates, logistic_function(years, *paramsDeath)) # Calculating the R^2 value
+print('R-Squared for births:', r2)
 
-#Create another list of the predicted data to 2122
-years = list(range(1960, 2123))
-predicted_data = []
-for i in range(2021, 2123):
-    predicted_data.append(np.exp(secondCoEff) * np.exp(firstCoEff * i))
-world_data = list(world_data)
-trained_years = list(range(1960, 2021))
-predicted_years = list(range(2021, 2123))
+predictionDeath = logistic_function(2122, *paramsDeath)
+print('Prediction for deaths in 2122:', round(predictionDeath*1000000), "people")
 
-#Fits the model within the parameters of matplotlib and shows it off
-plt.figure(figsize=(8, 6), dpi=80)
-plt.plot(trained_years, world_data, label='Actual')
-plt.plot(predicted_years, predicted_data, label='Predicted')
-plt.legend()
-plt.xlabel('Years')
-plt.ylabel('Population')
+
+"""plt.plot(years, birth_rates, label='Birth Rates')
 plt.show()
+plt.plot(years, death_rates, label='Death Rates')
+plt.show()
+"""
