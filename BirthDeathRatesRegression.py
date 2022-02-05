fa4a8f19d5b3c7665b06800aefd55a3f3fd50764
Grant-Stoehr <54612499+Grant-Stoehr@users.noreply.github.com>
2022-02-05 12:09:22 -0600

    Update BirthDeathRatesRegression.py

--- BirthDeathRatesRegression.py
+++ BirthDeathRatesRegression.py
@@ -8,6 +8,10 @@ import matplotlib.pyplot as plt
 def logistic_function(x, a, b, c, d):
     return a / (1. + np.exp(-c * (x - d))) + b
 
+# Source: https://calculator.academy/population-growth-calculator/
+def population_growth(population, growth_rate, time):
+    return (population * (pow(1 + growth_rate, time)))
+
 ##Reading in the data and cleaning it
 with open('BirthsandDeathRates.csv', 'r') as f:
      lines = f.readlines()
@@ -31,23 +35,36 @@ years = np.array(years).reshape(-1, 1).flatten()
 birth_rates = np.array(birth_rates).flatten()
 death_rates = np.array(death_rates).flatten()
 
-paramsBirth, covariance = curve_fit(logistic_function, years, birth_rates, p0=[2, 2000, .5, 2022]) # p0 is the initial guess for the parameters
+paramsBirth, covarianceBirth = curve_fit(logistic_function, years, birth_rates, p0=[120, 2000, 3, 2010], maxfev=10000) # p0 is the initial guess for the parameters
 r2 = r2_score(birth_rates, logistic_function(years, *paramsBirth)) # Calculating the R^2 value
 print('R-Squared for births:', r2)
 
 predictionBirth = logistic_function(2122, *paramsBirth)
 print('Prediction for births in 2122:', round(predictionBirth*1000000), "people")
 
-paramsDeath, covariance = curve_fit(logistic_function, years, birth_rates, p0=[2, 2000, .5, 2022]) # p0 is the initial guess for the parameters
-r2 = r2_score(birth_rates, logistic_function(years, *paramsDeath)) # Calculating the R^2 value
+paramsDeath, covarianceDeath = curve_fit(logistic_function, years, death_rates, p0=[50, 2000, 5, 2010], maxfev=10000) # p0 is the initial guess for the parameters
+r2 = r2_score(death_rates, logistic_function(years, *paramsDeath)) # Calculating the R^2 value
 print('R-Squared for births:', r2)
 
 predictionDeath = logistic_function(2122, *paramsDeath)
 print('Prediction for deaths in 2122:', round(predictionDeath*1000000), "people")
 
+# Final prediction for world population using birth
+# and death rates calculated using a logistic function
+forecasted_years = list(range(2021, 2122))
+forecasted_births = []
+forecasted_deaths = []
+for i in range(len(forecasted_years)):
+    if i == 2122:
+        break
+    forecasted_births.append(logistic_function(forecasted_years[i], *paramsBirth))
+    forecasted_deaths.append(logistic_function(forecasted_years[i], *paramsDeath))
 
-"""plt.plot(years, birth_rates, label='Birth Rates')
-plt.show()
-plt.plot(years, death_rates, label='Death Rates')
-plt.show()
-"""
+world_population = []
+world_population.append(7923230649) # current world population in 2021
+for i in range(len(forecasted_years)):
+    growthRate = (forecasted_births[i] - forecasted_deaths[i]) / world_population[i]
+    world_population.append(population_growth(world_population[i], growthRate, 1))
+
+
+print('Prediction for world population in 2122:', round(world_population[-1]), "people")
